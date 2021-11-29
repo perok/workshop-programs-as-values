df151c560943785890c7772cc516cdabc94231d4
Per Øyvind Kanestrøm <perokane@gmail.com>
2021-11-29 10:13:01 +0100

    Cleanup

--- modules/frontend/src/main/scala/frontend/Task.scala
+++ modules/frontend/src/main/scala/frontend/Task.scala
@@ -17,61 +17,16 @@ import frontend.domain.model.*
 
 class Task(world: World):
   def introduction(log: Log[IO]) =
-    val numberPattern = ".*Please call ([0-9]+)\\..*".r
-
-    for {
-      _ <- Helpers.ignore("Done")
-      number <- world.getPhone
-        .flatMap(phone => phone.call("81549300"))
-        .use(channel =>
-          channel.say("Good day") >>
-            channel.listen.take.flatMap(channel.say(_)) >>
-            channel.listen.take.flatMap {
-              case numberPattern(ma) =>
-                log.info(ma).as(ma)
-              case _ =>
-                log.info("Match failed").as("")
-            }
-        )
-      secret <- world.getPhone
-        .flatMap(phone => phone.call(number))
-        .use(channel => channel.say("Good day") >> Helpers.takeItAll(channel.listen).map(_.last))
-
-    } yield secret
+    Helpers.ignore("Not Yet")
 
   def task1(log: Log[IO]) =
     Helpers.ignore("Not Yet")
 
-  // Error handling
   def task2(log: Log[IO]) =
-    /* Test ideas:
-     *
-     *
-     *
-     * Call 100 people in range 7000-8000 at the same time and get the
-     * total sum they can give
-     *   -> traverse vs parTraverse
-     *
-     *
-     *
-     * */
-    List
-      // .range(49999, 51001)
-      .range(49999, 50111)
-      .parTraverse { number =>
-        world.getPhone.flatMap(_.call(number.toString)).use(_.say("How old are you?"))
-      }
-      .as("42")
+    Helpers.ignore("Not Yet")
 
   def task3(log: Log[IO]) =
-    Helpers.ignore("Not Yet") >>
-      (world.getPhone, world.getPhone).tupled.use { (phone1, phone2) =>
-        for {
-          _ <- (phone1.call("123478").use(_ => IO.sleep(3.seconds)).attempt,
-                phone2.call("82326434").use(channel => channel.say("Hey") >> IO.sleep(5.seconds))
-          ).parTupled
-        } yield ""
-      }
+    Helpers.ignore("Not Yet")
 
 object Helpers:
   def takeItAll(in: QueueSource[IO, String]): IO[List[String]] =
